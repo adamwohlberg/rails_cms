908b430e1302291ecb4c0b92c0c243ce1f4e0e50
adamzuckerberg <adam@adamzuckerberg.com>
2015-07-09 14:21:07 -0700

    Practice with render and redirects

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,21 @@
 class DemoController < ApplicationController
+  
+  layout false
+
   def index
+  	render('hello')
   end
+
+  def hello
+   	render('index')
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def white_house
+  	redirect_to('https://www.whitehouse.gov/')
+  end
+
 end
