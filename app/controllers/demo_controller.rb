92249a3a4ec84375cddb11f037c0c0c65472fcc9
adamzuckerberg <adam@adamzuckerberg.com>
2015-07-09 14:58:30 -0700

    Practice passing URL parameters

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,13 +3,12 @@ class DemoController < ApplicationController
   layout false
 
   def index
-  	render('hello')
   end
 
   def hello
-   	# render('index')
     @array = [1,2,3,4,5]
-    noray = [6,7,8,9,10]
+    @id = params[:id]
+    @page = params["page"]
   end
 
   def other_hello
