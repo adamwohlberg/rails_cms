cde166f1b7ee5e86636e5da0d629818f7cd4b7a6
adamzuckerberg <adam@adamzuckerberg.com>
2015-07-17 14:23:30 -0700

    Added Subjects/index.html.erb content

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'subjects/index'
 
-  get 'subjects/show'
-
-  get 'subjects/new'
-
-  get 'subjects/edit'
-
-  get 'subjects/delete'
 
   # get 'demo/index'
 
