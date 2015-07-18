f0f074e5d32163debc5ac860cf785b34ab2517f1
adamzuckerberg <adam@adamzuckerberg.com>
2015-07-18 09:43:46 -0700

    Created Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
 
+  get 'pages/index'
+
+  get 'pages/show'
+
+  get 'pages/new'
+
+  get 'pages/edit'
+
+  get 'pages/delete'
+
   # get 'demo/index'
 
   # root :to => "demo#index"
